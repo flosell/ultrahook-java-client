d0446e0de587256fc6c5efefe24c9f366fe57342
Florian Sellmayr <sellmayr@in.tum.de>
2013-11-17 12:24:46 +0100

    initial commit

---
+++ src/main/java/com/ultrahook/UltrahookClient.java
@@ -0,0 +1,20 @@
+package com.ultrahook;
+
+public class UltrahookClient {
+	private static final String VERSION = "0.1.2"; 
+
+	private final String key; 
+	private final String host; 
+	private final String version = VERSION;
+	
+	public UltrahookClient(String key, String host) {
+		super();
+		this.key = key;
+		this.host = host;
+	}
+	
+	public void connect() {
+		
+	}
+	
+}
